088b3309c6c4d5f7488d3439111eb56315b727f6
lalbahadurv <32303626+lalbahadurv@users.noreply.github.com>
2018-01-30 14:01:06 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+  
+app = Flask(__name__)
+  
+@app.route('/')
+def home():
+  return render_template('home.html')
+  
+@app.route('/about')
+def about():
+  return render_template('about.html')
+@app.route('/index')
+def index():
+	return render_template('index.html')
+@app.route('/signup')
+def signup():
+        return render_template('signup.html')
+
+  
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8080, debug=True)
